548a3f1a43878296f6d5de4fc8ebe7528b76e88d
MichaelUz <41979390+MichaelUz@users.noreply.github.com>
2019-12-12 19:52:30 -0500

    Add files via upload

---
+++ GameController/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static bool canSpawn = false;
+    public static bool canPlace = false;
+    [SerializeField] private GameObject overlayCanvas;
+
+    //Sets time Scale
+    public void SetTimeScale(float time)
+    {
+        Time.timeScale = time;
+    }
+
+    //Virtual Pause
+    public static void VirtualPause()
+    {
+        canPlace = false;
+        canSpawn = false;
+    }
+
+    //Virtual Play
+    public static void VirtualPlay(bool initial)
+    {
+        if (initial)
+        {
+            foreach (AttackerSpawner attackerSpawner in FindObjectsOfType<AttackerSpawner>())
+            {
+                attackerSpawner.SetStart(true);
+            }
+        }
+        canPlace = true;
+        canSpawn = true;
+    }
+}
+
+   
